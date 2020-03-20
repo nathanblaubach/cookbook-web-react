9d035787207bf9124cdf8fcd9aacdf38e05716eb
Nathan Blaubach <nathanblaubach@gmail.com>
2020-03-20 16:29:43 -0500

    Moved css for header to js

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,43 @@
 import React    from 'react';
 import { Link } from 'react-router-dom';
 
+const styles = {
+  header: {
+    height: '4rem',
+    padding: '.75rem',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  logo: {
+    width: '4rem'
+  },
+  nav: {
+    margin: '1.5rem 0'
+  },
+  links: {
+    marginLeft: '1rem',
+    color: '#00304E',
+    textDecoration: 'none',
+    fontWeight: 'bold'
+  }
+};
+
 const links = [
   { to: '/recipes', label: 'Search' },
   { to: '/about', label: 'About' }
 ];
 
 const Header = () => (
-  <header>
-    <img src={require("../images/logo.svg")} alt="logo" />
-    <nav>
-      { links.map((link, i) => <Link key={i} to={link.to}>{link.label}</Link>) }
+  <header style={styles.header}>
+    <img style={styles.logo} src={require("../images/logo.svg")} alt="logo" />
+    <nav style={styles.nav}>
+      { 
+        links.map((link, i) => 
+          <Link key={i} to={link.to} style={styles.links}>
+            {link.label}
+          </Link>
+        )
+      }
     </nav>
   </header>
 );
