e20d291ab8f4210e4456dac23c929a975f36e973
Nathan Blaubach <nathanblaubach@gmail.com>
2024-04-03 22:43:29 -0500

    Initialize tests for recipe queries

---
+++ api-dotnet/Cookbook/Cookbook.Tests/UseCases/Recipes/RecipeQueryTests.cs
@@ -0,0 +1,40 @@
+﻿using Cookbook.Domain.Interfaces;
+using Cookbook.Application.Queries;
+
+namespace Cookbook.Tests;
+
+public class RecipeQueryTests
+{
+    private readonly IRecipeQueries recipeQueries;
+
+    public RecipeQueryTests()
+    {
+        recipeQueries = new RecipeQueries(new TestDatabase());
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("  \n ")]
+    public async Task NullCategoriesWithNullOrWhitespaceStringsDoNotLimitResults(string searchTerm)
+    {
+        // Act
+        var results = await recipeQueries.GetByParamsAsync(searchTerm, null);
+
+        // Assert
+        Assert.NotEmpty(results);
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("  \n ")]
+    public async Task EmptyCategoriesWithNullOrWhitespaceStringsDoNotLimitResults(string searchTerm)
+    {
+        // Act
+        var results = await recipeQueries.GetByParamsAsync(searchTerm, []);
+
+        // Assert
+        Assert.NotEmpty(results);
+    }
+}
