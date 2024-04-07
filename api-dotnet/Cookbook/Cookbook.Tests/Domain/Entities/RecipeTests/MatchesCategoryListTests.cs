dfd1afd64ef649768fbe40bc28410f2be67a5ee9
Nathan Blaubach <nathanblaubach@gmail.com>
2024-04-06 20:52:43 -0500

    Add Category List Filter Tests

---
+++ api-dotnet/Cookbook/Cookbook.Tests/Domain/Entities/RecipeTests/MatchesCategoryListTests.cs
@@ -0,0 +1,69 @@
+﻿using Cookbook.Domain.Entities;
+
+namespace Cookbook.Tests;
+
+public class MatchesCategoryListTests
+{
+    [Fact]
+    public void ReturnsMatched_WhenCategoryListIsNull()
+    {
+        // Arrange
+        var recipe = GetBaseRecipe();
+
+        // Act
+        var matched = recipe.MatchesCategoryList(null);
+
+        // Assert
+        Assert.True(matched);
+    }
+
+    [Fact]
+    public void ReturnsMatched_WhenCategoryListIsEmpty()
+    {
+        // Arrange
+        var recipe = GetBaseRecipe();
+
+        // Act
+        var matched = recipe.MatchesCategoryList([]);
+
+        // Assert
+        Assert.True(matched);
+    }
+
+    
+
+    [Fact]
+    public void ReturnsMatched_WhenCategoryListContainsId()
+    {
+        // Arrange
+        var recipe = GetBaseRecipe();
+
+        // Act
+        var matched = recipe.MatchesCategoryList([123, 342, 545, 2468]);
+
+        // Assert
+        Assert.True(matched);
+    }
+
+    [Fact]
+    public void ReturnsNotMatched_WhenCategoryListDoesNotContainId()
+    {
+        // Arrange
+        var recipe = GetBaseRecipe();
+
+        // Act
+        var matched = recipe.MatchesCategoryList([123, 342, 545]);
+
+        // Assert
+        Assert.False(matched);
+    }
+
+    private static Recipe GetBaseRecipe() => new()
+    {
+        Id = 1234L,
+        Name = "Recipe Name",
+        Ingredients = [ "Ingredient 1", "Ingredient 2" ],
+        Instructions = [ "Instruction 1", "Instruction 2" ],
+        CategoryId = 2468L,
+    };
+}
