0e4fd78d3174f1ccd38203d75214d5091d8040ac
Nathan Blaubach <nathanblaubach@gmail.com>
2024-05-25 11:13:49 -0500

    Fix link to logo file

--- web-react/src/components/Page/Page.tsx
+++ web-react/src/components/Page/Page.tsx
@@ -1,6 +1,6 @@
 import React  from 'react';
 import { Link } from 'react-router-dom';
-import cookbookLogo from '../assets/logo.svg';
+import cookbookLogo from '../../assets/logo.svg';
 import './Page.css';
 
 type PageProps = {
