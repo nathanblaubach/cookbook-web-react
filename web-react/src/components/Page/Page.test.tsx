53667eaf19c90dd3521c2341c86fec1d42aaaaa5
Nathan Blaubach <nathanblaubach@gmail.com>
2024-06-12 22:07:06 -0500

    Break down into smaller tests

--- web-react/src/components/Page/Page.test.tsx
+++ web-react/src/components/Page/Page.test.tsx
@@ -5,23 +5,28 @@ import { BrowserRouter } from 'react-router-dom';
 
 describe('Page', () => {
 
-  it('displays cookbook logo, links and children', () => {
+  // Arrange and Act
+  const component = render(
+    <BrowserRouter>
+      <Page>
+        <p>This is a child</p>
+      </Page>
+    </BrowserRouter>
+  );
 
-    // Arrange and Act
-    const component = render(
-      <BrowserRouter>
-        <Page>
-          <p>This is a child</p>
-        </Page>
-      </BrowserRouter>
-    );
-
+  it('should display cookbook logo', () => {
     // Assert
     expect(component.getByRole('img', { name: 'McClain Family Cookbook Logo' })).toBeDefined();
+  });
 
+  it('should display navigation links', () => {
+    // Assert
     const navigation = within(component.getByRole('navigation'));
     expect(navigation.getAllByRole('link').length).toBeGreaterThan(0);
+  });
 
+  it('should display children', () => {
+    // Assert
     expect(component.getByRole('paragraph').textContent).toBe("This is a child");
   });
 
--- web-react/src/components/RecipeCard/RecipeCard.test.tsx
+++ web-react/src/components/RecipeCard/RecipeCard.test.tsx
@@ -4,37 +4,42 @@ import { describe, expect, it } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('RecipeCard', () => {
-  it('renders recipe name, ingredients and link correctly', () => {
 
-    // Arrange
-    const recipe: RecipeCardProps = {
-      id: 736,
-      name: 'Pasta Carbonara',
-      relevantIngredients: ['Pasta', 'Eggs', 'Bacon', 'Parmesan Cheese'],
-    };
-  
-    // Act
-    const component = render(
-      <BrowserRouter>
-        <RecipeCard {...recipe} />
-      </BrowserRouter>
-    );
+  // Arrange
+  const recipe: RecipeCardProps = {
+    id: 736,
+    name: 'Pasta Carbonara',
+    relevantIngredients: ['Pasta', 'Eggs', 'Bacon', 'Parmesan Cheese'],
+  };
 
+  // Act
+  const component = render(
+    <BrowserRouter>
+      <RecipeCard {...recipe} />
+    </BrowserRouter>
+  );
+
+  it('should display the recipe name', () => {
     // Assert
     const displayedName = component
       .getByRole('heading')
       .textContent;
     expect(displayedName).toBe(recipe.name);
+  });
 
+  it('should display the recipe ingredients', () => {
+    // Assert
     const displayedIngredients = component
       .getAllByRole('paragraph')
       .map(element => element.textContent);
     expect(displayedIngredients).toEqual(recipe.relevantIngredients);
-    
+  });
+
+  it('should display the recipe link', () => {
+    // Assert
     const displayedUrl = component
       .getByRole('link')
       .getAttribute('href');
     expect(displayedUrl).toContain(recipe.id);
-
   });
 });
