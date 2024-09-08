57730dcdd012823243b5a6a863e636401be12f04
Nathan Blaubach <nathanblaubach@gmail.com>
2024-09-08 14:54:38 -0500

    Implement filtering by category

--- web-minimal/src/js/use-cases.js
+++ web-minimal/src/js/use-cases.js
@@ -2,19 +2,26 @@
  * Filter recipes based on the search term.
  * @param {Object[]} recipes
  * @param {string | null | undefined} searchTerm
+ * @param {string[] | null | undefined} selectedCategories
  * @returns the filtered recipes
  */
-function filterRecipes(recipes, searchTerm) {
+function filterRecipes(recipes, searchTerm, selectedCategories) {
   const normalizedSearchTerm = (searchTerm || "").trim().toLowerCase();
-  if (normalizedSearchTerm === "") {
+  const normalizedSelectedCategories = selectedCategories || [];
+  if (normalizedSearchTerm === "" && normalizedSelectedCategories.length === 0) {
     return recipes;
   }
-  return recipes.filter(recipe =>
-    recipe.name.toLowerCase().includes(normalizedSearchTerm) ||
+  return recipes.filter(recipe => 
+    (normalizedSearchTerm === "" || searchTermMatchesRecipeNameOrIngredients(recipe, normalizedSearchTerm)) &&
+    (normalizedSelectedCategories.length === 0 || normalizedSelectedCategories.includes(recipe.category))
+  );
+}
+
+function searchTermMatchesRecipeNameOrIngredients(recipe, normalizedSearchTerm) {
+  return recipe.name.toLowerCase().includes(normalizedSearchTerm) ||
     recipe.ingredients.some((ingredient) =>
       ingredient.toLowerCase().includes(normalizedSearchTerm),
-    )
-  );
+    );
 }
 
 if (typeof module !== "undefined") {
--- web-minimal/test/use-cases.test.js
+++ web-minimal/test/use-cases.test.js
@@ -13,40 +13,55 @@ describe("use-cases", () => {
             {
                 id: 0,
                 name: "Chocolate Cake",
+                category: "Dessert",
                 ingredients: ["chocolate", "flour", "sugar"]
             },
             {
                 id: 1,
                 name: "Chicken Alfredo",
+                category: "Main Course",
                 ingredients: ["chicken", "pasta", "sauce"]
             },
             {
                 id: 2,
                 name: "Peanut Butter Cookies",
+                category: "Dessert",
                 ingredients: ["peanut butter", "flour", "sugar"]
             }
         ];
 
-        it("should not filter any recipes when no search term is given", () => {
+        it("should not filter any recipes when neither search term nor selected categories are given", () => {
             [
-                undefined,
-                null,
-                "",
-                " ",
-                "  ",
-            ].forEach(searchTerm =>
-                assert(filterRecipes(recipes, searchTerm).length === recipes.length)
-            );
+                { searchTerm: undefined, selectedCategories: undefined },
+                { searchTerm: null, selectedCategories: undefined },
+                { searchTerm: "", selectedCategories: undefined },
+                { searchTerm: " ", selectedCategories: undefined },
+                { searchTerm: "  ", selectedCategories: undefined },
+                { searchTerm: undefined, selectedCategories: null },
+                { searchTerm: null, selectedCategories: null },
+                { searchTerm: "", selectedCategories: null },
+                { searchTerm: " ", selectedCategories: null },
+                { searchTerm: "  ", selectedCategories: null },
+                { searchTerm: undefined, selectedCategories: [] },
+                { searchTerm: null, selectedCategories: [] },
+                { searchTerm: "", selectedCategories: [] },
+                { searchTerm: " ", selectedCategories: [] },
+                { searchTerm: "  ", selectedCategories: [] },
+            ].forEach(({ searchTerm, selectedCategories }) => {
+                assert(filterRecipes(recipes, searchTerm, selectedCategories).length === recipes.length)
+            });
         });
 
         it("should only include results that have the search term in their name or ingredients", () => {
-
-            assert(listsAreEqual(filterRecipes(recipes, "chicken").map(recipe => recipe.id), [1]));
-            assert(listsAreEqual(filterRecipes(recipes, "cookie").map(recipe => recipe.id), [2]));
-            assert(listsAreEqual(filterRecipes(recipes, "sugar").map(recipe => recipe.id), [0, 2]));
-
+            assert(listsAreEqual(filterRecipes(recipes, "chicken", undefined).map(recipe => recipe.id), [1]));
+            assert(listsAreEqual(filterRecipes(recipes, "cookie", undefined).map(recipe => recipe.id), [2]));
+            assert(listsAreEqual(filterRecipes(recipes, "sugar", undefined).map(recipe => recipe.id), [0, 2]));
         });
 
+        it("should only include results that have the selected categories", () => {
+            assert(listsAreEqual(filterRecipes(recipes, undefined, "Main Course").map(recipe => recipe.id), [1]));
+            assert(listsAreEqual(filterRecipes(recipes, undefined, "Dessert").map(recipe => recipe.id), [0, 2]));
+        });
     });
 
 });
