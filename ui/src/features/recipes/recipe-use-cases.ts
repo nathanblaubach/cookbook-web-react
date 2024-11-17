a2501ddbdbaf91895f1123b967ced7234c32879b
Nathan Blaubach <nathanblaubach@gmail.com>
2024-11-17 09:08:03 -0600

    Use the new presentation mappers

--- ui/src/features/recipes/recipe-use-cases.ts
+++ ui/src/features/recipes/recipe-use-cases.ts
@@ -1,45 +1,35 @@
 import { CardContent } from "../../components/CardGrid/CardGrid";
 import { FilterItem } from "../../components/Filter/Filter";
 import { RecipeRepository } from "./recipe-repository";
+import {mapRecipeToCardContent} from "./mappers/mapRecipeToCardContent.ts";
+import {mapCategoryToFilterItem} from "./mappers/mapCategoryToFilterItem.ts";
 
 export class RecipeUseCases {
 
-  constructor(private readonly repository: RecipeRepository) {}
+    constructor(private readonly repository: RecipeRepository) {}
 
-  public getRecipeCards(searchTerm: string, categoryFilters: FilterItem[]): CardContent[] {
-    const ingredientSearchActive: boolean = searchTerm.length >= 3;
+    public getRecipeCards(searchTerm: string, categoryFilters: FilterItem[]): CardContent[] {
+        const ingredientSearchActive: boolean = searchTerm.length >= 3;
 
-    const activeCategories: string[] = categoryFilters.reduce(
-      (acc: string[], filter: FilterItem): string[] => {
-        if (filter.checked) {
-          acc.push(filter.id);
-        }
-        return acc; 
-      },
-      []
-    );
+        const activeCategories: string[] = categoryFilters.reduce(
+            (acc: string[], filter: FilterItem): string[] => {
+                if (filter.checked) {
+                    acc.push(filter.id);
+                }
+                return acc;
+            },
+            []
+        );
 
-    return this.repository
-      .getRecipesBySearchTermAndCategories(searchTerm, activeCategories)
-      .map(recipe => {
-        const relevantIngredients = !ingredientSearchActive ? [] : recipe.ingredients
-          .filter((ingredient: string) => ingredient.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
+        return this.repository
+            .getRecipesBySearchTermAndCategories(searchTerm, activeCategories)
+            .map(recipe => mapRecipeToCardContent(recipe, ingredientSearchActive ? searchTerm : undefined));
+    }
 
-        return {
-          id: recipe.id,
-          link: `/recipes/${recipe.id}`,
-          title: recipe.name,
-          contentLines: relevantIngredients
-        }
-      });
-  }
-
-  public getCategoryFilterItems(): FilterItem[] {
-    return this.repository.getCategories().map(category => ({
-      id: category,
-      name: category,
-      checked: false
-    }));
-  }
+    public getCategoryFilterItems(): FilterItem[] {
+        return this.repository
+            .getCategories()
+            .map(mapCategoryToFilterItem);
+    }
 
 }
