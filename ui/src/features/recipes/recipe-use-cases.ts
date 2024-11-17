73b0b7a75fb362b3798c067ca59a99674c7b4d88
Nathan Blaubach <nathanblaubach@gmail.com>
2024-11-17 09:36:01 -0600

    Use the function for getting checked categories from the filter items

--- ui/src/features/recipes/recipe-use-cases.ts
+++ ui/src/features/recipes/recipe-use-cases.ts
@@ -3,27 +3,16 @@ import { FilterItem } from "../../components/Filter/Filter";
 import { RecipeRepository } from "./recipe-repository";
 import {mapRecipeToCardContent} from "./mappers/mapRecipeToCardContent.ts";
 import {mapCategoryToFilterItem} from "./mappers/mapCategoryToFilterItem.ts";
+import {getCheckedFilterItemIds} from "../../common/getCheckedFilterItemIds.ts";
 
 export class RecipeUseCases {
 
     constructor(private readonly repository: RecipeRepository) {}
 
     public getRecipeCards(searchTerm: string, categoryFilters: FilterItem[]): CardContent[] {
-        const ingredientSearchActive: boolean = searchTerm.length >= 3;
-
-        const activeCategories: string[] = categoryFilters.reduce(
-            (acc: string[], filter: FilterItem): string[] => {
-                if (filter.checked) {
-                    acc.push(filter.id);
-                }
-                return acc;
-            },
-            []
-        );
-
         return this.repository
-            .getRecipesBySearchTermAndCategories(searchTerm, activeCategories)
-            .map(recipe => mapRecipeToCardContent(recipe, ingredientSearchActive ? searchTerm : undefined));
+            .getRecipesBySearchTermAndCategories(searchTerm, getCheckedFilterItemIds(categoryFilters))
+            .map(recipe => mapRecipeToCardContent(recipe, searchTerm.length >= 3 ? searchTerm : undefined));
     }
 
     public getCategoryFilterItems(): FilterItem[] {
