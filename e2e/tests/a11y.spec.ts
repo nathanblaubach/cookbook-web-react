e8f8c605ccb7bb857ac48b6c16803c5c3cd48cdf
Nathan Blaubach <nathanblaubach@gmail.com>
2024-11-19 19:53:59 -0600

    Expand filter dropdown for recipe page a11y test and add test for when the recipe is not found

--- e2e/tests/a11y.spec.ts
+++ e2e/tests/a11y.spec.ts
@@ -16,22 +16,29 @@ test('Recipe Search Page (base url)', async ({ page }) => {
 
 test('Recipe Search Page', async ({ page }) => {
   await page.goto(`${getBaseUrl()}/recipes`);
-  await expect(page).toHaveTitle(/McClain Family Cookbook/);
+  await page.getByRole('button').click(); // Expand filter area to ensure it is accessible
+
   await expect(page.getByRole('heading', { name: 'Recipes' })).toBeVisible();
+  await expect(page.getByRole('checkbox', { name: 'Beverage' })).toBeVisible();
   await expectNoAccessibilityViolation(page);
 });
 
-test('Recipe Page', async ({ page }) => {
+test('Recipe Page: Found', async ({ page }) => {
   const recipeId = 0;
   await page.goto(`${getBaseUrl()}/recipes/${recipeId}`);
-  await expect(page).toHaveTitle(/McClain Family Cookbook/);
   await expect(page.getByRole('heading', { name: 'Recipe Name' })).toBeVisible();
+  await expectNoAccessibilityViolation(page);
+});
+
+test('Recipe Page: Not Found', async ({ page }) => {
+  const recipeId = -1;
+  await page.goto(`${getBaseUrl()}/recipes/${recipeId}`);
+  await expect(page.getByRole('heading', { name: 'Recipe Not Found' })).toBeVisible();
   await expectNoAccessibilityViolation(page);
 });
 
 test('About Page', async ({ page }) => {
   await page.goto(`${getBaseUrl()}/about`);
-  await expect(page).toHaveTitle(/McClain Family Cookbook/);
   await expect(page.getByRole('heading', { name: 'The McClain Family Cookbook' })).toBeVisible();
   await expectNoAccessibilityViolation(page);
 });
